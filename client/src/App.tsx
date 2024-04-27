b77667fd321c73c120e81475f9231581b31146f8
salidamaharjan <salidamaharjan@outlook.com>
2024-04-27 13:51:05 -0500

    Setup Shadcn ui library

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
-  return <div className="text-green-400">Post It</div>;
+  return (
+    <div className="text-green-400">
+      Post It
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
