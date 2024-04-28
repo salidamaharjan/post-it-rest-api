fc7464f69e48399ae2d77ea75a990d2ab46fe471
salidamaharjan <salidamaharjan@outlook.com>
2024-04-27 21:10:07 -0500

    Move post to PostPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,61 +1,14 @@
 import "./App.css";
-import { useEffect, useState } from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
-type Post = {
-  id: number;
-  title: string;
-  content: string;
-  clientId: number;
-  createdAt: string;
-  updatedAt: string;
-};
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [allPosts, setAllPosts] = useState<Post[]>([]);
-  useEffect(() => {
-    fetchData();
-  }, []);
-  async function fetchData() {
-    try {
-      const response = await fetch("http://localhost:3000/api/posts");
-      const result = await response.json();
-      setAllPosts(result);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
   return (
-    <div className="flex flex-col p-4 gap-2">
-      <div className="text-green-600 text-2xl text-center font-bold">Posts</div>
-      <div className="flex flex-col gap-4">
-        {allPosts.map((post) => {
-          return (
-            <Card>
-              <CardHeader>
-                <CardTitle>{post.title}</CardTitle>
-                <CardDescription>By id {post.clientId}</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div className="text-green-700 text-lg font-bold">Content</div>
-                <p>{post.content}</p>
-              </CardContent>
-              <CardFooter className="flex justify-between">
-                <p>Created at {post.createdAt}</p>
-                <p>Updated at {post.updatedAt}</p>
-              </CardFooter>
-            </Card>
-          );
-        })}
-      </div>
+    <div>
+      <nav>
+        <div>Posts</div>
+        <div>Login</div>
+      </nav>
+      <Outlet />
     </div>
   );
 }
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,12 +2,18 @@ import { createBrowserRouter } from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage";
 import App from "./App";
+import PostPage from "./pages/PostPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
+      {
+        path: "/",
+        element: <PostPage />,
+        index: true,
+      },
       {
         path: "/login",
         element: <LoginPage />,
