5fa17260ff87f1f466cd91074bb66672976912b8
salidamaharjan <salidamaharjan@outlook.com>
2024-04-27 21:51:56 -0500

    update login page ui

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -4,14 +4,18 @@ import { Label } from "@/components/ui/label";
 
 function LoginPage() {
   return (
-    <div>
-      <Card className="m-4 rounded-md">
-        <CardContent>
-          <div>Login</div>
-          <Label htmlFor="username">Username</Label>
-          <Input placeholder="Username" />
-          <Label htmlFor="password">Password</Label>
-          <Input placeholder="Password" />
+    <div className="flex justify-center mt-6">
+      <Card className="m-6 rounded-md p-4 w-[500px]">
+        <CardContent className="flex flex-col gap-6">
+          <div className="font-bold text-lg text-green-600">Login</div>
+          <div>
+            <Label htmlFor="username">Username</Label>
+            <Input placeholder="Username" />
+          </div>
+          <div>
+            <Label htmlFor="password">Password</Label>
+            <Input placeholder="Password" />
+          </div>
         </CardContent>
       </Card>
     </div>
