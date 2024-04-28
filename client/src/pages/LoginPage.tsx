ae9c6197c1e01534878c0d4db54668a6bcba95ce
salidamaharjan <salidamaharjan@outlook.com>
2024-04-27 21:42:31 -0500

    Add input to create login form

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,4 +1,16 @@
+import { Input } from "@/components/ui/input";
+import { Card, CardContent } from "@/components/ui/card";
 function LoginPage() {
-  return <div className="font-bold text-2xl text-black">Login</div>;
+  return (
+    <div>
+      <Card className="m-4 rounded-md">
+        <CardContent >
+          <div>Login</div>
+          <Input placeholder="Username" />
+          <Input placeholder="Password" />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 export default LoginPage;
