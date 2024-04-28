ef4172016fecff9138f8ffa8091ed34b811d5747
salidamaharjan <salidamaharjan@outlook.com>
2024-04-27 21:45:14 -0500

    Use label from shadcn ui library

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,12 +1,16 @@
 import { Input } from "@/components/ui/input";
 import { Card, CardContent } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+
 function LoginPage() {
   return (
     <div>
       <Card className="m-4 rounded-md">
-        <CardContent >
+        <CardContent>
           <div>Login</div>
+          <Label htmlFor="username">Username</Label>
           <Input placeholder="Username" />
+          <Label htmlFor="password">Password</Label>
           <Input placeholder="Password" />
         </CardContent>
       </Card>
