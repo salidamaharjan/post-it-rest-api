98d1fd08b45533b54ebfaa7bb4fc409aca483023
salidamaharjan <salidamaharjan@outlook.com>
2024-04-28 09:36:55 -0500

    Use useState to set username and passwrod

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -2,8 +2,14 @@ import { Input } from "@/components/ui/input";
 import { Card, CardContent } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  function handleOnClick() {
+    alert(`username: ${username} password: ${password}`);
+  }
   return (
     <div className="flex justify-center mt-6">
       <Card className="m-6 rounded-md p-4 w-[500px]">
@@ -11,13 +17,28 @@ function LoginPage() {
           <div className="font-bold text-lg text-green-600">Login</div>
           <div>
             <Label htmlFor="username">Username</Label>
-            <Input placeholder="Username" />
+            <Input
+              placeholder="Username"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
           </div>
           <div>
             <Label htmlFor="password">Password</Label>
-            <Input placeholder="Password" />
+            <Input
+              placeholder="Password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
           </div>
-          <Button className="bg-green-500 text-lg font-bold">Login</Button>
+          <Button
+            className="bg-green-500 text-lg font-bold"
+            onClick={handleOnClick}
+          >
+            Login
+          </Button>
         </CardContent>
       </Card>
     </div>
