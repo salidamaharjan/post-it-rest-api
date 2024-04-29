60db48930ad3e9248da3705d68f1df744b361c53
salidamaharjan <salidamaharjan@outlook.com>
2024-04-28 20:03:05 -0500

    Use UserContext in router

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -3,11 +3,16 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import App from "./App";
 import PostPage from "./pages/PostPage";
+import { UserContextProvider } from "./context/UserContext";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: (
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    ),
     children: [
       {
         path: "/",
