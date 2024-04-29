5746937bc10d5bd2b2e4f0e4287ab0d02bd8e6a5
salidamaharjan <salidamaharjan@outlook.com>
2024-04-28 20:31:00 -0500

    Use usercontext in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,31 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "./context/UserContext";
 function App() {
+  const userLoggedInState = useContext(UserContext);
   return (
-    <div >
+    <div>
       <Tabs defaultValue="account">
         <div className="flex flex-col">
-        <TabsList>
-          <TabsTrigger value="posts">
-            <Link to="/">Posts</Link>
-          </TabsTrigger>
-          <TabsTrigger value="login">
-            <Link to="/login">Login</Link>
-          </TabsTrigger>
-          <TabsTrigger value="logout">Logout</TabsTrigger>
-        </TabsList>
+          {userLoggedInState?.isLoggedIn === true ? (
+            <TabsList>
+              <TabsTrigger value="posts">
+                <Link to="/">Posts</Link>
+              </TabsTrigger>
+              <TabsTrigger value="logout">Logout</TabsTrigger>
+            </TabsList>
+          ) : (
+            <TabsList>
+              <TabsTrigger value="posts">
+                <Link to="/">Posts</Link>
+              </TabsTrigger>
+              <TabsTrigger value="login">
+                <Link to="/login">Login</Link>
+              </TabsTrigger>
+            </TabsList>
+          )}
         </div>
         <Outlet />
       </Tabs>
